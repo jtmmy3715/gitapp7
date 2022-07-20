450e1be5ab17192bed53d3365b929e9ac17ebeb6
jtmmy3715 <jtmmy3715@gmail.com>
2022-07-20 17:59:45 +0800

    HAHAWorld

--- src/main/java/MyHelloWorld.java
+++ src/main/java/MyHelloWorld.java
@@ -29,6 +29,7 @@ public class MyHelloWorld extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
 
 	/**
